72fd7205ea896c3518df409760b6d8d7d26048af
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2023-06-08 19:16:34 +0800

    adding custom edit text with drawing logic

--- app/src/main/java/com/example/myapplication/CustomEditText.java
+++ app/src/main/java/com/example/myapplication/CustomEditText.java
@@ -2,21 +2,33 @@ package com.example.myapplication;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.text.Editable;
+import android.text.Layout;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.StaticLayout;
+import android.text.TextPaint;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
 
 @SuppressLint("AppCompatCustomView")
 public class CustomEditText extends EditText {
     private static final String TAG = "CustomEditText";
     private boolean mUpdatingText;
+    private StaticLayout mStaticLayout = null;
+    private TextPaint mTextPaint;
+    // private String mText = "\u200b";
+    private String mText = "X";
 
     public CustomEditText(Context context) {
         super(context);
@@ -41,20 +53,116 @@ public class CustomEditText extends EditText {
         addTextChangedListener(new TextWatcherDelegator());
         setInitialState();
     }
+
     private void setInitialState() {
-        mUpdatingText = true;
-        SpannableString string = new SpannableString("\u200b");
-        string.setSpan(new CustomLineHeightSpan(300), 0, string.length(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
-        setText(string, TextView.BufferType.SPANNABLE);
-        mUpdatingText = false;
+        mTextPaint = new TextPaint();
+        mTextPaint.setAntiAlias(true);
+        mTextPaint.setTextSize(16 * getResources().getDisplayMetrics().density);
+        mTextPaint.setColor(0xFF000000);
+
+        // default to a single line of text
+        int width = (int) mTextPaint.measureText(mText);
+        mStaticLayout = new StaticLayout(mText, mTextPaint, (int) width, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0, false);
+
+        // New API alternate
+        //
+        // StaticLayout.Builder builder = StaticLayout.Builder.obtain(mText, 0, mText.length(), mTextPaint, width)
+        //        .setAlignment(Layout.Alignment.ALIGN_NORMAL)
+        //        .setLineSpacing(0, 1) // add, multiplier
+        //        .setIncludePad(false);
+        // mStaticLayout = builder.build();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        // Tell the parent layout how big this view would like to be
+        // but still respect any requirements (measure specs) that are passed down.
+
+        // determine the width
+        int width;
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthRequirement = MeasureSpec.getSize(widthMeasureSpec);
+        if (widthMode == MeasureSpec.EXACTLY) {
+            width = widthRequirement;
+        } else {
+            width = mStaticLayout.getWidth() + getPaddingLeft() + getPaddingRight();
+            if (widthMode == MeasureSpec.AT_MOST) {
+                if (width > widthRequirement) {
+                    width = widthRequirement;
+                    // too long for a single line so relayout as multiline
+                    mStaticLayout = new StaticLayout(mText, mTextPaint, width, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0, false);
+                }
+            }
+        }
+
+        // determine the height
+        int height;
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightRequirement = MeasureSpec.getSize(heightMeasureSpec);
+        if (heightMode == MeasureSpec.EXACTLY) {
+            height = heightRequirement;
+        } else {
+            height = mStaticLayout.getHeight() + getPaddingTop() + getPaddingBottom();
+            if (heightMode == MeasureSpec.AT_MOST) {
+                height = Math.min(height, heightRequirement);
+            }
+        }
+
+        // Required call: set width and height
+        setMeasuredDimension(width, height);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // super.onDraw(canvas);
+        // https://stackoverflow.com/a/6017003/7295772
+        if (getText().length() == 0) {
+            Paint paint = new Paint();
+            Path path = new Path();
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.TRANSPARENT);
+            canvas.drawPaint(paint);
+            for (int i = 50; i < 100; i++) {
+                path.moveTo(i, i-1);
+                path.lineTo(i, i);
+            }
+            path.close();
+            paint.setStrokeWidth(30);
+            paint.setPathEffect(null);
+            paint.setColor(Color.BLACK);
+            paint.setStyle(Paint.Style.STROKE);
+            canvas.drawPath(path, paint);
+            Path highlightPath = new Path();
+            // highlightPath.moveTo(0,100);
+            highlightPath.lineTo(500, 0);
+            highlightPath.close();
+            getLayout().getCursorPath(0, highlightPath, getText());
+            // Paint paint = new Paint();
+            paint.setColor(Color.BLUE);
+
+            // do as little as possible inside onDraw to improve performance
+
+            // draw the text on the canvas after adjusting for padding
+            canvas.save();
+            canvas.translate(getPaddingLeft(), getPaddingTop() + 500);
+            getLayout().draw(canvas, highlightPath, paint, 0);
+            // mStaticLayout.draw(canvas);
+            canvas.restore();
+        } else {
+            // getLayout().draw(canvas);
+            super.onDraw(canvas);
+        }
     }
 
     private void maybeSetText(CharSequence s) {
-        Spannable span = (Spannable)getText();
+        // Spannable span = (Spannable)getText();
+        requestLayout();
+        invalidate();
         if (s.length() == 0) {
-            setInitialState();
+            // setInitialState();
         } else {
-            span.setSpan(new CustomLineHeightSpan(300), 0, span.length(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+            // span.setSpan(new CustomLineHeightSpan(300), 0, span.length(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
         }
     }
 
---
+++ app/src/main/java/com/example/myapplication/MongolTextView.java
@@ -0,0 +1,193 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.text.Layout;
+import android.text.Spannable;
+import android.text.Spanned;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+@SuppressLint("AppCompatCustomView")
+public class MongolTextView extends EditText {
+
+    private TextPaint textPaint;
+    private Paint cursorPaint = new Paint();
+    private boolean mCursorIsVisible;
+    private CursorTouchLocationListener listener;
+
+    // Naming is based on pre-rotated/mirrored values
+    private float mCursorBaseY;
+    private float mCursorBottomY;
+    private float mCursorAscentY; // This is a negative number
+    private float mCursorX;
+
+    private static final float CURSOR_THICKNESS = 2f;
+    private int mCursorHeightY;
+
+    // Constructors
+    public MongolTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public MongolTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MongolTextView(Context context) {
+        super(context);
+        init();
+    }
+
+    // This class requires the mirrored Mongolian font to be in the assets/fonts folder
+    private void init() {
+
+        Spannable span = (Spannable)getText();
+        span.setSpan(new CustomLineHeightSpan(300), 0, span.length(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+        setText(span, BufferType.SPANNABLE);
+        this.mCursorIsVisible = true;
+
+        cursorPaint.setStrokeWidth(CURSOR_THICKNESS);
+        cursorPaint.setColor(Color.BLACK); // TODO should be same as text color
+
+    }
+
+    // This interface may be deleted if touch functionality is not needed
+    public interface CursorTouchLocationListener {
+
+        /**
+         * Returns the touch location to be used for the cursor so you can update the insert
+         * location in a text string.
+         *
+         * @param glyphIndex You will need to translate glyphIndex into a Unicode index if you are using a
+         *                   Unicode string.
+         */
+        public void onCursorTouchLocationChanged(int glyphIndex);
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // swap the height and width
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), getMeasuredHeight());
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // super.onDraw(canvas);
+        textPaint = getPaint();
+        textPaint.setColor(getCurrentTextColor());
+        textPaint.drawableState = getDrawableState();
+
+        canvas.save();
+
+        // canvas.translate(0,  500);
+
+        // draw the cursor
+        if (mCursorIsVisible) {
+            setCursorLocation(7);
+            canvas.drawLine(mCursorX, mCursorBottomY, mCursorX, mCursorBaseY + mCursorAscentY,
+                    cursorPaint);
+        }
+
+        // here create a copy of the layout
+        // add a span
+        // and draw on that layout
+        Layout newLayout = new StaticLayout(getText(), textPaint, getLayout().getWidth(), Layout.Alignment.ALIGN_NORMAL, 1.0f, 0, false);
+        newLayout.draw(canvas);
+
+        canvas.restore();
+    }
+
+    public void showCursor(boolean visible) {
+        mCursorIsVisible = visible;
+        this.invalidate();
+        // TODO make the cursor blink
+    }
+
+    public void setCursorColor(int color) {
+        cursorPaint.setColor(color);
+    }
+
+    public void setCursorLocation(int characterOffset) {
+
+        Layout layout = this.getLayout();
+
+        if (layout != null) {
+
+            try {
+                // This method is giving a lot of crashes so just surrounding with
+                // try catch for now
+
+                int line = layout.getLineForOffset(characterOffset);
+                mCursorX = layout.getPrimaryHorizontal(characterOffset);
+                mCursorBaseY = layout.getLineBaseline(line);
+                mCursorBottomY = layout.getLineBottom(line);
+                mCursorAscentY = layout.getLineAscent(line);
+
+                this.invalidate();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public class InputWindowTouchListener implements OnTouchListener {
+        @Override
+        public boolean onTouch(View view, MotionEvent event) {
+
+            Layout layout = ((TextView) view).getLayout();
+
+            // swapping x and y for touch events
+            int y = (int) event.getX();
+            int x = (int) event.getY();
+
+            if (layout != null) {
+
+                int line = layout.getLineForVertical(y);
+                int offset = layout.getOffsetForHorizontal(line, x);
+
+                mCursorX = layout.getPrimaryHorizontal(offset);
+                mCursorBaseY = layout.getLineBaseline(line);
+                mCursorBottomY = layout.getLineBottom(line);
+                mCursorAscentY = layout.getLineAscent(line);
+                mCursorHeightY = layout.getLineTop(line);
+
+                view.invalidate();
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        //handler.postDelayed(mLongPressed, 1000);
+                        listener.onCursorTouchLocationChanged(offset);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        //handler.removeCallbacks(mLongPressed);
+                        // notify the host activity of the new cursor location
+
+                        break;
+                }
+
+            }
+
+            return false;
+        }
+
+    }
+
+    public void setCursorTouchLocationListener(CursorTouchLocationListener listener) {
+        this.listener = listener;
+    }
+}
