98751f974276f9fb87d4e1817b5b123bf0ef30f2
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2023-07-06 21:51:27 +0800

    adding first test case

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -1,17 +1,31 @@
 package com.example.myapplication;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
 
 import static org.junit.Assert.*;
+import android.content.Context;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(RobolectricTestRunner.class)
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void create() {
+        Context context = ApplicationProvider.getApplicationContext();
+        CustomEditText editText = new CustomEditText(context);
+        assertEquals(editText.onTextContextMenuItem(0), true);;
     }
 }
