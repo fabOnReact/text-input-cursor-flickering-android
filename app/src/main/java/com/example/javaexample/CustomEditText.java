31db5796c6abbd17ffaa87de1663c38604d1da33
Fabrizio Bertoglio <fabrizio.bertoglio@gmail.com>
2023-11-09 09:21:59 +0800

    clean up project

--- app/src/main/java/com/example/javaexample/CustomEditText.java
+++ app/src/main/java/com/example/javaexample/CustomEditText.java
@@ -30,61 +30,15 @@ public class CustomEditText extends EditText {
     public CustomEditText(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         // setTextDirection(TEXT_DIRECTION_LTR);
-        init();
     }
 
     public CustomEditText(Context context, AttributeSet attrs) {
         super(context, attrs);
         // setTextDirection(TEXT_DIRECTION_LTR);
-        init();
     }
 
     public CustomEditText(Context context) {
         super(context);
         // setTextDirection(TEXT_DIRECTION_LTR);
-    }
-
-    private void init() {
-        // do nothing
-    }
-
-    private void maybeSetText(CharSequence s) {
-        // do nothing
-    }
-
-    private class TextWatcherDelegator implements TextWatcher {
-
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            // do nothing
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-            // maybeSetText
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-            // do nothing
-        }
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        // do nothing
-    }
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        // do nothing
-    }
-
-    @Override
-    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
-        // do nothing
-        super.onFocusChanged(focused, direction, previouslyFocusedRect);
     }
 }
